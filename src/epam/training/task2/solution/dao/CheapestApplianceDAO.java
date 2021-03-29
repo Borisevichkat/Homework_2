f3ae8140c4d6c77a7c4d3af738497b8e2f26b6ff
Borisevichkat <52928737+Borisevichkat@users.noreply.github.com>
2021-03-30 00:16:57 +0300

    Delete src/epam/training/task2/solution directory

--- src/epam/training/task2/solution/dao/CheapestApplianceDAO.java
+++
@@ -1,5 +0,0 @@
-package epam.training.task2.solution.dao;
-
-public interface CheapestApplianceDAO {
-    String findCheapestAppliance() throws DAOException;
-}
--- src/epam/training/task2/solution/dao/ReadApplianceDAO.java
+++
@@ -1,8 +0,0 @@
-package epam.training.task2.solution.dao;
-
-import java.util.ArrayList;
-
-public interface ReadApplianceDAO {
-    ArrayList<String> readApplianceData() throws DAOException;
-    ArrayList<String> readApplianceDataByCategory(String categoryName) throws DAOException;
-}
